d495144dbd378352903e89fb9b795370e35736b4
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2020-09-27 03:36:04 +0200

    Update to Voilà 0.2.x (#1)
    Add .gitignore and use special branch w/submodule.
    
    Update requirement to 2020.9.27.
    
    Use updated matcloud voila template.
    Update copy_voila_template.py to copy the now 2 directories to the
    correct places.

--- copy_voila_template.py
+++ copy_voila_template.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import os
 import json
+from pathlib import Path
 import shutil
 import sys
+from typing import Tuple
 
 
-def get_voila_templates_dir() -> str:
+def get_voila_templates_dir() -> Tuple[Path, Path]:
     """Find absolute path to Jupyter's data dir that contains Voilà templates"""
     jupyter_dirs = os.popen("jupyter --paths --json")
     try:
@@ -17,15 +19,21 @@ def get_voila_templates_dir() -> str:
         )
 
     for data_dir in jupyter_dirs.get("data", []):
-        voila_templates_dir = os.path.join(data_dir, "voila/templates/")
-        if os.path.exists(voila_templates_dir):
+        data_dir = Path(data_dir)
+        nbconvert_templates_dir = data_dir / "nbconvert/templates/"
+        voila_templates_dir = data_dir / "voila/templates/"
+        if voila_templates_dir.exists():
             break
     else:
         for data_dir in jupyter_dirs.get("data", []):
             if "conda" in data_dir:
+                data_dir = Path(data_dir)
                 # Create "voila/templates/" in this virtualenv-specific config folder
-                voila_templates_dir = os.path.join(data_dir, "voila/templates/")
-                os.mkdir(voila_templates_dir)
+                voila_templates_dir = data_dir / "voila/templates/"
+                voila_templates_dir.mkdir(parents=True, exist_ok=True)
+                # Create "nbconvert/templates/" in this virtualenv-specific config folder
+                nbconvert_templates_dir = data_dir / "nbconvert/templates/"
+                nbconvert_templates_dir.mkdir(parents=True, exist_ok=True)
                 break
         else:
             raise RuntimeError(
@@ -33,27 +41,33 @@ def get_voila_templates_dir() -> str:
                 "Also could not find 'conda' in any of data directories."
             )
 
-    return os.path.abspath(voila_templates_dir)
+    return voila_templates_dir.resolve(), nbconvert_templates_dir.resolve()
 
 
 def copy_template(name: str):
     """Copy a Voilà template to Jupyter's first (and best) data dir"""
-    src = os.path.abspath(name)
-    if not os.path.exists(src):
-        raise RuntimeError(f"Can not find provided template {name!r} at {src} !")
+    src_voila = Path(f"voila_{name}").resolve()
+    src_nbconvert = Path(f"nbconvert_{name}").resolve()
 
-    dest = get_voila_templates_dir()
-    new_dest = os.path.join(dest, name)
-    if os.path.exists(new_dest):
-        print(
-            f"Template {name!r} has already been copied into {dest}, "
-            "will first remove it, then copy it anew !"
-        )
-        shutil.rmtree(new_dest)
-        print(f"Successfully removed {dest} !")
+    for src in (src_voila, src_nbconvert):
+        if not src.exists():
+            raise RuntimeError(f"Can not find provided template {name!r} at {src} !")
 
-    shutil.copytree(src, new_dest, symlinks=False)
-    print(f"Successfully copied {name!r} to {dest} !")
+    dest_voila, dest_nbconvert = get_voila_templates_dir()
+
+    for dest in (dest_voila, dest_nbconvert):
+        if dest.joinpath(name).exists():
+            print(
+                f"Template {name!r} has already been copied into {dest}, "
+                "will first remove it, then copy it anew !"
+            )
+            shutil.rmtree(str(dest / name))
+            print(f"Successfully removed {dest / name} !")
+
+    for src, dest in zip((src_voila, src_nbconvert), (dest_voila, dest_nbconvert)):
+        print(f"Will now copy {src} to {dest / name}.")
+        shutil.copytree(src, dest / name, symlinks=False)
+        print(f"Successfully copied {name!r} to {dest} !")
 
 
 if __name__ == "__main__":
@@ -66,7 +80,6 @@ if __name__ == "__main__":
     try:
         copy_template(template)
     except Exception as exc:  # pylint: disable=broad-except
-        print(f"Exception while copying template!\nException: {exc}")
-        sys.exit(1)
+        sys.exit(f"ERROR while copying template: {exc}")
     else:
         sys.exit(0)
